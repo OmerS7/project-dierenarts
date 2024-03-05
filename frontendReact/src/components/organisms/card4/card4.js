34bf7009da61cc0c08f8f6447d18c87fbe561451
Omer Seker <otseker@hotmail.com>
2024-03-05 15:05:31 +0100

    changed line

--- frontendReact/src/components/organisms/card4/card4.js
+++ frontendReact/src/components/organisms/card4/card4.js
@@ -3,31 +3,31 @@ import PropTypes from 'prop-types';
 import './_card4.style.scss';
 
 const Card4 = ({ title, enteredData }) => {
-    return (
-      <div className="Card4">
-        <h2 className="card-title">{title}</h2>
-        <div className="entered-data">
-          <ul>
-            {Object.entries(enteredData).map(([key, value]) => (
-              <li key={key}>
-                <strong>{key}:</strong> {value ? (
-                  Object.entries(value).map(([subKey, subValue]) => (
-                    <div key={subKey}>
-                      <strong>{subKey}:</strong> {subValue !== null ? subValue : "Not specified"}
-                    </div>
-                  ))
-                ) : "Not specified"}
-              </li>
-            ))}
-          </ul>
-        </div>
+  return (
+    <div className="Card4">
+      <h2 className="card-title">{title}</h2>
+      <div className="entered-data">
+        <ul>
+          {Object.entries(enteredData).map(([key, value]) => (
+            <li key={key}>
+              <strong>{key}:</strong> {value ? (
+                Object.entries(value).map(([subKey, subValue]) => (
+                  <div key={subKey}>
+                    <strong>{subKey}:</strong> {subValue === null ? "Not specified" : subValue}
+                  </div>
+                ))
+              ) : "Not specified"}
+            </li>
+          ))}
+        </ul>
       </div>
-    );
-  };
-  
-  Card4.propTypes = {
-    title: PropTypes.string.isRequired,
-    enteredData: PropTypes.object.isRequired,
-  };
-  
-  export default Card4;
+    </div>
+  );
+};
+
+Card4.propTypes = {
+  title: PropTypes.string.isRequired,
+  enteredData: PropTypes.object.isRequired,
+};
+
+export default Card4;
